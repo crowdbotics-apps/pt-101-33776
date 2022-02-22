a2419338e43682db89800b4fd8910400f128d345
Crowdbotics <team@crowdbotics.com>
2022-02-22 02:20:45 +0000

    Update options

--- options/options.js
+++ options/options.js
@@ -38,11 +38,13 @@ const styles = StyleSheet.create({
     fontSize: 16
   }
 })
+
 export const globalOptions = {
   name: "pt_101_33776",
   url: "https://pt_101_33776.botics.co",
   api: "https://pt_101_33776.botics.co/api/v1"
 }
+
 export const modulesOptions = {
   "@modules/app-menu": {
     title: "Menu",
